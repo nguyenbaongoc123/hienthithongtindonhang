f55321b3e3d640fdb427d5a7e7902cec1691ae95
nguyenbaongoc123 <127759913+nguyenbaongoc123@users.noreply.github.com>
2023-03-31 21:46:22 +0900

    Create hienthithongtindonhang.py

---
+++ hienthithongtindonhang.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+n = int(input("Nhap vao so tien: "))
+if n>= 150:
+    print(f'so tien can phai thanh toan la {n-50}')
+elif n>=100 :
+    print(f'so tien can phai thanh toan la {n-25}')
+elif n>=75 :
+    print(f'so tien can phai thanh toan la {n-15}')
+else:
+    print(f'so tien can phai thanh toan la {n}')
+
+
+# In[ ]:
+
+
+
+
